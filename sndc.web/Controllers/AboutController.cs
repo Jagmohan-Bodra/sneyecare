f0f8f84aa2241e4621e818eefb576a6b278c3cf9
Jagmohan Bodra <jagmohan.bodra@gmail.com>
2024-12-06 14:55:36 +0530

    first commit project

---
+++ sndc.web/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sndc.web.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+    }
+}
